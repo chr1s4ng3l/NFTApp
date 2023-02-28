ad3e0a73299c7309e6e2aa67d9fd322c232a6aa0
Consultant <christopher.tamayocazarez@gmail.com>
2023-02-28 12:30:27 -0500

    ViewModel

--- app/src/main/java/com/example/nftapp/viewmodel/NftViewModel.kt
+++ app/src/main/java/com/example/nftapp/viewmodel/NftViewModel.kt
@@ -1,9 +1,39 @@
 package com.example.nftapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.nftapp.model.Asset
+import com.example.nftapp.model.domain.AssetsDomain
+import com.example.nftapp.rest.NftRepository
+import com.example.nftapp.utils.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 @HiltViewModel
-class NftViewModel: ViewModel() {
+class NftViewModel @Inject constructor(private val nftRepository: NftRepository) : ViewModel() {
+
+    var selectedAsset: Asset? = null
+
+    private val _asset: MutableStateFlow<UIState<List<AssetsDomain>>> =
+        MutableStateFlow(UIState.LOADING)
+    val asset: StateFlow<UIState<List<AssetsDomain>>> get() = _asset
+
+
+    init {
+        getAsset()
+
+    }
+
+    private fun getAsset() {
+        viewModelScope.launch {
+            nftRepository.getAssets().collect(){
+                _asset.value = it
+            }
+        }
+    }
 }
